30566c9387eace6652dcf61f89c7be1f380c664e
Łukasz Boczoń <lukasz.boczon@gotoma.pl>
2024-10-13 16:03:16 +0200

    Updates

---
+++ assets/Home-tx1ZYVGu.js
@@ -0,0 +1,1 @@
+import{j as e,N as t}from"./index-DFbAGrl7.js";import{u as r}from"./useSettingsDb-B8bP60Yk.js";function u(){const{language:s,level:o}=r();return s?o?e.jsx(t,{to:"/categories"}):e.jsx(t,{to:"/levels"}):e.jsx(t,{to:"/languages"})}export{u as default};
---
+++ assets/NoMatch-CkPCBeZT.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DFbAGrl7.js";function o(){return t.jsx(t.Fragment,{children:"NoMatch"})}export{o as default};
